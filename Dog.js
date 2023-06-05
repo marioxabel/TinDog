15a3d5663f06c3d1d1dcbeeb8b5beb59c36e2052
marioxabel <31260739+marioxabel@users.noreply.github.com>
2023-06-05 12:15:36 -0600

    v.1

--- Dog.js
+++ Dog.js
@@ -6,10 +6,12 @@ class Dog {
     }
     
     getDogHtml() {
-        const {name, avatar, age, bio} = this
+        const {name, avatar, age, bio, hasBeenSwiped, hasBeenLiked} = this
         return `
         <div id="profile" class="profile">
         <img class="profile-pic" src="./${avatar}">
+        <img class="badge-like ${hasBeenSwiped && hasBeenLiked ? "" : "hidden"}" src="./images/badge-like.png">
+        <img class="badge-nope ${hasBeenSwiped && !hasBeenLiked ? "" : "hidden"}" src="./images/badge-nope.png">
         <div class="summary">
                 <h1>${name}, ${age}</h1>
                 <h2>${bio}</h2>
